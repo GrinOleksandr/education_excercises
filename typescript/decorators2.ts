5571ad1479e52c306b5396cf9f81558338f8964f
Sasha Grin <aleksander.g@ideasoft.io>
2022-11-03 09:42:35 +0200

    topic: "TypeScript Decorators"

---
+++ typescript/decorators2.ts
@@ -0,0 +1,34 @@
+// 2. Create decorator to catch errors returned from the child class
+//@ts-
+@CreatedAt
+class UserService2 implements IUserService {
+  users: number = 1000;
+
+  @Catch(true)
+  getUsersInDatabase(): number {
+    return this.users;
+  }
+}
+
+function Catch(rethrow: boolean = false) {
+  return (
+    target: Object,
+    propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<(...args: any[]) => any>,
+  ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
+    const method = descriptor.value;
+
+    descriptor.value = async (...args: any[]) => {
+      try {
+        return method?.apply(target, args);
+      } catch (e) {
+        if (e instanceof Error) {
+          console.log(e.message);
+          if (rethrow) {
+            throw e;
+          }
+        }
+      }
+    };
+  };
+}
